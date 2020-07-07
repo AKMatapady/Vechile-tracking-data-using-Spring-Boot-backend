951a94d6035b021b14916f73258774214ad44288
AKM_Curious <amithkmbvr@gmail.com>
2020-07-06 19:50:27 -0500

    update controller file

--- src/main/java/per/akmatapady/controller/TrackerController.java
+++ src/main/java/per/akmatapady/controller/TrackerController.java
@@ -1,11 +1,16 @@
 package per.akmatapady.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import per.akmatapady.entity.Alert;
+import per.akmatapady.entity.Location;
 import per.akmatapady.entity.Vehicle;
 import per.akmatapady.entity.VehicleReading;
 import per.akmatapady.service.TrackerService;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -14,18 +19,42 @@ import java.util.List;
 public class TrackerController {
 
     @Autowired
+    @Qualifier("trackerServiceImpl")
     private TrackerService trackerService;
 
     @PutMapping(value = "vehicles")
-    private List<Vehicle> addVehicles(@RequestBody List<Vehicle> vehicleList)
+    public List<Vehicle> addVehicles(@RequestBody List<Vehicle> vehicleList)
     {
         return trackerService.addVehicles(vehicleList);
     }
 
     @PostMapping(value = "readings")
-    private VehicleReading addReading(@RequestBody VehicleReading vehicleReading)
+    public VehicleReading addReading(@RequestBody VehicleReading vehicleReading)
     {
         return trackerService.addReadings(vehicleReading);
+    }
+
+    @GetMapping(value = "vehicles")
+    public List<Vehicle> findAllVehicles()
+    {
+        return trackerService.findAllVehicles();
+    }
+
+    @GetMapping(value = "alert/{id}")
+    public List<Alert> findAllAlerts(@PathVariable("id") String vin)
+    {
+        return trackerService.findAllAlerts(vin);
+    }
+
+    @GetMapping(value = "alert/high")
+    public List<Alert> findHighAlert(){
+        return trackerService.findHighPriority();
+    }
+
+    @GetMapping(value = "location/{id}")
+    public List<Location> findLocation(@PathVariable("id") String vin)
+    {
+        return trackerService.findLocation(vin);
     }
 
 }
