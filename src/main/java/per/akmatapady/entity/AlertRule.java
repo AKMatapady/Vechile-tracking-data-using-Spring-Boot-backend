0453a9012ceccf4b82e533ec8fac71c76652df15
AKM_Curious <amithkmbvr@gmail.com>
2020-07-06 19:42:21 -0500

    update the AlertRule and Severity classes

--- src/main/java/per/akmatapady/entity/AlertRule.java
+++ src/main/java/per/akmatapady/entity/AlertRule.java
@@ -2,5 +2,18 @@ package per.akmatapady.entity;
 
 public enum AlertRule
 {
-    RPM_TOO_HIGH, LOW_FUEL, INCORRECT_TIRE_PRESSURE, ENGINE_CHECK_REQUIRED
+    RPM_TOO_HIGH("HIGN_RPM"),
+    LOW_FUEL("LOW_FUEL"),
+    INCORRECT_TIRE_PRESSURE("INCORRECT_TIRE_PRESSURE"),
+    ENGINE_CHECK_REQUIRED("CHECK_ENGINE");
+
+    private String action;
+
+    public String getAction() {
+        return action;
+    }
+
+    private AlertRule(String action) {
+        this.action = action;
+    }
 }
--- src/main/java/per/akmatapady/entity/Severity.java
+++ src/main/java/per/akmatapady/entity/Severity.java
@@ -2,5 +2,15 @@ package per.akmatapady.entity;
 
 public enum Severity
 {
-    HIGH, MEDIUM, LOW
+    HIGH("HIGH"), MEDIUM("MEDIUM"), LOW("LOW");
+
+    private String action;
+
+    public String getAction() {
+        return action;
+    }
+
+    Severity(String action) {
+        this.action = action;
+    }
 }
