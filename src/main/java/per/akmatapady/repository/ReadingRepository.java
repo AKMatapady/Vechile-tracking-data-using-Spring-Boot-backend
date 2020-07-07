9c54bfd29b024ff89861632766f9795a49055c7e
AKM_Curious <amithkmbvr@gmail.com>
2020-07-06 19:43:37 -0500

    update ReadingRepository class

--- src/main/java/per/akmatapady/repository/ReadingRepository.java
+++ src/main/java/per/akmatapady/repository/ReadingRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import per.akmatapady.entity.VehicleReading;
 import per.akmatapady.entity.VehicleReadingID;
 
+import java.util.List;
+
 public interface ReadingRepository extends CrudRepository<VehicleReading, VehicleReadingID> {
+    List<VehicleReading> findByVin(String vin);
 }
